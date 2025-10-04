626ae1c627951dcad5ae49ec630f1555c8e4e56c
Boxxxx <muhammadadlifirmansyah0@gmail.com>
2025-10-04 11:19:38 +0700

    Title

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SMPN 02 - Web</title>
+    <title>SMPN 02 Gunungputri - Web</title>
 
     {{-- CSS Bootstrap & Font --}}
     <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}">
