4434b44eb4f28a6003a398c94313e6c8ccf54eb1
4spvviar <pejuangbot1@gmail.com>
2025-10-04 14:06:32 +0700

    visi dan misi

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,22 +15,40 @@ class UserController extends Controller
     //
     public function index()
     {
-        $data ['guru'] = guru::all();
-        $data ['siswa'] = siswa::whereIn('tahun_masuk',[2023, 2024, 2025])->get();
-        $data ['ekskul'] = ekstrakurikuler::all();
+        $data['guru'] = guru::all();
+        $data['siswa'] = siswa::whereIn('tahun_masuk', [2023, 2024, 2025])->get();
+        $data['ekskul'] = ekstrakurikuler::all();
         $data['berita'] = Berita::with('user') // Mengambil relasi user
-        ->orderBy('id_berita', 'desc') // Urutkan berdasarkan id_berita terbaru
-        ->take(5) // Ambil 5 berita saja
-        ->get();
-        $data ['galeri'] = galeri::orderBy('id_galeri', 'asc')->take(6)->get();
+            ->orderBy('id_berita', 'desc') // Urutkan berdasarkan id_berita terbaru
+            ->take(5) // Ambil 5 berita saja
+            ->get();
+        $data['galeri'] = galeri::orderBy('id_galeri', 'asc')->take(6)->get();
         return view('halamanUtama', $data);
     }
 
     public function info()
-    {
-        $ps = profil_sekolah::all();
-        return view('profilSekolah', compact('ps'));
-    }
+{
+    $psRaw = profil_sekolah::all();
+
+    $ps = $psRaw->map(function ($item) {
+        $lines = preg_split('/\r\n|\r|\n/', trim($item->visi_misi));
+
+        // Ambil baris pertama sebagai visi
+        $visi = isset($lines[0]) ? trim($lines[0], '"') : '';
+
+        // Sisanya anggap sebagai misi
+        $misiLines = array_slice($lines, 1);
+        $misi = implode("\n", $misiLines); // Gabung lagi jadi string
+
+        $item->visi = $visi;
+        $item->misi = $misi;
+
+        return $item;
+    });
+
+    return view('profilSekolah', compact('ps'));
+}
+
 
     public function berita()
     {
@@ -39,7 +57,7 @@ class UserController extends Controller
     }
     public function Galeri()
     {
-        $galeri = galeri::all();    
+        $galeri = galeri::all();
         return view('galeri', compact('galeri'));
     }
     public function Ekskul()
--- resources/views/profilSekolah.blade.php
+++ resources/views/profilSekolah.blade.php
@@ -10,88 +10,97 @@
         <div class="mt-5">
             <div class="row align-items-center">
                 <div class="col-md-4 text-center mb-3 mb-md-0">
-                    <img src="{{ asset('assets/image/sj.jpg') }}" 
-                         alt="Foto Sejarah Sekolah" 
-                         class="img-fluid rounded shadow-sm w-100" 
+                    <img src="{{ asset('assets/image/sj.jpg') }}"
+                         alt="Foto Sejarah Sekolah"
+                         class="img-fluid rounded shadow-sm w-100"
                          style="max-width: 500px;">
                 </div>
                 <div class="col-md-8">
                     <h5 class="fw-semibold mb-4">Sejarah Sekolah</h5>
                     <p class="text-justify">
                         Sekolah ini didirikan pada tahun 1993 dengan tujuan memberikan pendidikan berkualitas bagi masyarakat sekitar.
-                        Pada awal berdirinya, sekolah hanya memiliki 12 ruang kelas dan staff pengajar yang terbatas. Seiring berjalannya waktu, 
-                        dengan dedikasi dan kerja keras semua pihak, sekolah ini berkembang menjadi salah satu institusi pendidikan 
-                        unggulan di wilayahnya. Banyak prestasi telah diraih, baik di bidang akademik maupun non-akademik, yang turut 
-                        mengharumkan nama sekolah di tingkat daerah hingga nasional.
+                        Pada awal berdirinya, sekolah hanya memiliki 12 ruang kelas dan staf pengajar yang terbatas.
+                        Seiring berjalannya waktu, dengan dedikasi dan kerja keras semua pihak, sekolah ini berkembang menjadi
+                        salah satu institusi pendidikan unggulan di wilayahnya. Banyak prestasi telah diraih, baik di bidang
+                        akademik maupun non-akademik, yang turut mengharumkan nama sekolah di tingkat daerah hingga nasional.
                     </p>
                 </div>
             </div>
         </div>
-        
+
         @if ($ps->count() > 0)
-        
-        {{-- Data Informasi Profil Sekolah --}}
-        @foreach ($ps as $item)
-        <h6 class="mt-3 fw-semibold text-primary">Visi</h6>
-        <p class="text-justify">{{ $item->visi_misi }}</p>
-            <table class="table table-borderless align-middle table-striped mt-4">
-                <tbody>
-                    <tr>
-                        <th class="text-start">Nama Sekolah</th>
-                        <td class="text-end">{{ $item->nama_sekolah }}</td>
-                    </tr>
-                    <tr>
-                        <th class="text-start">Kepala Sekolah</th>
-                        <td class="text-end">{{ $item->kepala_sekolah }}</td>
-                    </tr>
-                    <tr>
-                        <th class="text-start">Foto Sekolah</th>
-                        <td class="text-end">
-                            @if ($item->foto)
-                                <img src="{{ asset('storage/' . $item->foto) }}" 
-                                     class="img-fluid rounded shadow-sm"
-                                     width="120" alt="Foto Sekolah">
-                            @else
-                                <span>-</span>
-                            @endif
-                        </td>
-                    </tr>
-                    <tr>
-                        <th class="text-start">Logo Sekolah</th>
-                        <td class="text-end">
-                            @if ($item->logo)
-                                <img src="{{ asset('storage/' . $item->logo) }}" 
-                                     class="rounded-circle shadow-sm"
-                                     width="80" alt="Logo Sekolah">
-                            @else
-                                <span>-</span>
-                            @endif
-                        </td>
-                    </tr>
-                    <tr>
-                        <th class="text-start">NPSN</th>
-                        <td class="text-end">{{ $item->npsn }}</td>
-                    </tr>
-                    <tr>
-                        <th class="text-start">Alamat</th>
-                        <td class="text-end">{{ $item->alamat }}</td>
-                    </tr>
-                    <tr>
-                        <th class="text-start">Kontak</th>
-                        <td class="text-end">{{ $item->kontak }}</td>
-                    </tr>
-                    <tr>
-                        <th class="text-start">Tahun Berdiri</th>
-                        <td class="text-end">{{ $item->tahun_berdiri }}</td>
-                    </tr>
-                    <tr>
-                        <th class="text-start">Deskripsi</th>
-                        <td class="text-end long-text">{{ $item->deskripsi }}</td>
-                    </tr>
-                </tbody>
-            </table>
-        @endforeach
-          @else
+            {{-- Data Informasi Profil Sekolah --}}
+            @foreach ($ps as $item)
+
+                {{-- Visi --}}
+                <h6 class="mt-4 fw-semibold text-primary">Visi</h6>
+                <p class="text-justify">{{ $item->visi }}</p>
+
+                @if (!empty($item->misi))
+                    <h6 class="mt-3 fw-semibold text-primary">Misi</h6>
+                    <p class="text-justify">{{ $item->misi }}</p>
+                @endif
+
+                {{-- Detail Sekolah --}}
+                <table class="table table-borderless align-middle table-striped mt-4">
+                    <tbody>
+                        <tr>
+                            <th class="text-start">Nama Sekolah</th>
+                            <td class="text-end">{{ $item->nama_sekolah }}</td>
+                        </tr>
+                        <tr>
+                            <th class="text-start">Kepala Sekolah</th>
+                            <td class="text-end">{{ $item->kepala_sekolah }}</td>
+                        </tr>
+                        <tr>
+                            <th class="text-start">Foto Sekolah</th>
+                            <td class="text-end">
+                                @if ($item->foto)
+                                    <img src="{{ asset('storage/' . $item->foto) }}"
+                                         class="img-fluid rounded shadow-sm"
+                                         width="120" alt="Foto Sekolah">
+                                @else
+                                    <span>-</span>
+                                @endif
+                            </td>
+                        </tr>
+                        <tr>
+                            <th class="text-start">Logo Sekolah</th>
+                            <td class="text-end">
+                                @if ($item->logo)
+                                    <img src="{{ asset('storage/' . $item->logo) }}"
+                                         class="rounded-circle shadow-sm"
+                                         width="80" alt="Logo Sekolah">
+                                @else
+                                    <span>-</span>
+                                @endif
+                            </td>
+                        </tr>
+                        <tr>
+                            <th class="text-start">NPSN</th>
+                            <td class="text-end">{{ $item->npsn }}</td>
+                        </tr>
+                        <tr>
+                            <th class="text-start">Alamat</th>
+                            <td class="text-end">{{ $item->alamat }}</td>
+                        </tr>
+                        <tr>
+                            <th class="text-start">Kontak</th>
+                            <td class="text-end">{{ $item->kontak }}</td>
+                        </tr>
+                        <tr>
+                            <th class="text-start">Tahun Berdiri</th>
+                            <td class="text-end">{{ $item->tahun_berdiri }}</td>
+                        </tr>
+                        <tr>
+                            <th class="text-start">Deskripsi</th>
+                            <td class="text-end long-text">{{ $item->deskripsi }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            @endforeach
+
+        @else
             <div class="alert alert-warning text-center mt-4" role="alert">
                 Belum ada Informasi Profil Sekolah yang diinput.
             </div>
